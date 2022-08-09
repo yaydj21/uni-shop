3157f60f08afb9cd603c89955d4d8e3edbdbc729
yaydj21 <594571058@qq.com>
2022-08-09 20:25:59 +0800

    完成搜索功能开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,28 +1,38 @@
 <template>
-	<view class="scroll-view-container">
-		<!-- 左侧滑动区域 -->
-		<scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
-			<block v-for="(item,i) in cateList" :key="i">
-				<view :class="['left-scroll-view-item', i===active ? 'active' : '']" @click="activeChanged(i)">{{item.cat_name}}</view>
-			</block>
-		</scroll-view>
-		<!-- 右侧滑动区域 -->
-		<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
-			<view class="cate-lv2" v-for="(item2,i2) in cateLevel2" :key="i2">
-				<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
-				<!-- 当前二级分类下的三级分类列表 -->
-				<view class="cate-lv3-list">
-					<!-- 三级分类的item项 -->
-					<view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
-						<!-- 三级分类的图片 -->
-						<image :src="item3.cat_icon.replace('dev','web')"></image>
-						<!-- 三级分类的文本 -->
-						<text>{{item3.cat_name}}</text>
+	<!--  -->
+	<view>
+		<!-- 使用自定义的搜索组件 -->
+		<!-- <my-search :bgcolor="'black'" :radius="3"></my-search> -->
+		<my-search @click="gotoSearch"></my-search>
+		<view class="scroll-view-container">
+			<!-- 左侧滑动区域 -->
+			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+				<block v-for="(item,i) in cateList" :key="i">
+					<view :class="['left-scroll-view-item', i===active ? 'active' : '']" @click="activeChanged(i)">
+						{{item.cat_name}}
+					</view>
+				</block>
+			</scroll-view>
+			<!-- 右侧滑动区域 -->
+			<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+				<view class="cate-lv2" v-for="(item2,i2) in cateLevel2" :key="i2">
+					<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+					<!-- 当前二级分类下的三级分类列表 -->
+					<view class="cate-lv3-list">
+						<!-- 三级分类的item项 -->
+						<view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3"
+							@click="gotoGoodsList(item3)">
+							<!-- 三级分类的图片 -->
+							<image :src="item3.cat_icon.replace('dev','web')"></image>
+							<!-- 三级分类的文本 -->
+							<text>{{item3.cat_name}}</text>
+						</view>
 					</view>
 				</view>
-			</view>
-		</scroll-view>
+			</scroll-view>
+		</view>
 	</view>
+
 </template>
 
 <script>
@@ -32,11 +42,11 @@
 				// 当前设备可用宽度
 				wh: 0,
 				cateList: [],
-				active:0,
+				active: 0,
 				// 二级分类的列表
-				cateLevel2:[],
+				cateLevel2: [],
 				// 滚动条距离
-				scrollTop:0
+				scrollTop: 0
 			}
 		},
 		methods: {
@@ -51,23 +61,28 @@
 					return uni.$showMsg();
 				}
 				this.cateList = res.message;
-				
+
 				// 为二级分类赋值
 				this.cateLevel2 = res.message[0].children;
 			},
-			activeChanged(i){
+			activeChanged(i) {
 				this.active = i;
 				// 重新为二级分类赋值
 				this.cateLevel2 = this.cateList[i].children;
-				
+
 				// this.scrollTop = this.scrollTop ===0?1:0;
 			},
 			// 跳转到商品列表页面
-			gotoGoodsList(item){
+			gotoGoodsList(item) {
 				uni.navigateTo({
-					url:'/subpkg/goods_list/goods_list?cid='+item.cat_id
+					url: '/subpkg/goods_list/goods_list?cid=' + item.cat_id
 				})
-			}
+			},
+			gotoSearch() {
+				uni.navigateTo({
+					url:'/subpkg/search/search'
+				})
+			},
 		},
 		onLoad() {
 			// 获取当前设备的信息
@@ -111,28 +126,33 @@
 			}
 		}
 	}
-	.cate-lv2-title{
+
+	.cate-lv2-title {
 		font-size: 12px;
 		font-weight: bold;
 		text-align: center;
 		padding: 15px 0;
 	}
-	.cate-lv3-list{
+
+	.cate-lv3-list {
 		display: flex;
 		flex-wrap: wrap;
-		.cate-lv3-item{
+
+		.cate-lv3-item {
 			width: 33.3%;
 			display: flex;
 			flex-flow: column;
 			justify-content: center;
 			align-items: center;
 			margin-bottom: 10px;
-			image{
+
+			image {
 				width: 60px;
 				height: 60px;
-				
+
 			}
-			text{
+
+			text {
 				font-size: 12px;
 			}
 		}
